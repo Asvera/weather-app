25e91aef33a8335b4801caf41091fa56fafc8f18
Shubham yadav <122096194+Asvera@users.noreply.github.com>
2023-02-15 22:03:43 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,22 +9,12 @@ const wrapper = document.querySelector(".wrapper"),
 
 let api;
 
-//PLEASE PUT YOUR API KEY HERE
 let apiKey = "b190a0605344cc4f3af08d0dd473dd25";
 
 inputField.addEventListener("keyup", (e) => {
   // if user pressed enter btn and input value is not empty
   if (e.key == "Enter" && inputField.value != "") {
     requestApi(inputField.value);
-  }
-});
-
-locationBtn.addEventListener("click", () => {
-  if (navigator.geolocation) {
-    // if browser support geolocation api
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-  } else {
-    alert("Your browser not support geolocation api");
   }
 });
 
@@ -105,64 +95,3 @@ function weatherDetails(info) {
 arrowBack.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
-
-//change color theme
-//get the theme from local storage
-getTheme();
-
-//color palette
-const colors = [
-  "hsl(345, 80%, 50%)",
-  "hsl(100, 80%, 50%)",
-  "hsl(200, 80%, 50%)",
-  "hsl(227, 66%, 55%)",
-  "hsl(26, 80%, 50%)",
-  "hsl(44, 90%, 51%)",
-  "hsl(280, 100%, 65%)",
-  "hsl(480, 100%, 25%)",
-  "hsl(180, 100%, 25%)",
-];
-
-const colorBtns = document.querySelectorAll(".theme-color");
-const darkModeBtn = document.querySelector(".dark-mode-btn");
-
-//change theme to dark
-var isDark = false;
-darkModeBtn.addEventListener("click", () => {
-  if(!isDark) {
-    changeTheme("#000");
-    isDark = true;
-  }else{
-    changeTheme(colors[3]);
-    isDark = false;
-  }
-});
-
-//loop through colors array and set each color to a button
-for (let i = 0; i < colorBtns.length; i++) {
-  colorBtns[i].style.backgroundColor = colors[i];
-}
-
-colorBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    changeTheme(btn.style.backgroundColor);
-  });
-});
-
-function changeTheme(color) {
-  document.documentElement.style.setProperty("--primary-color", color);
-  saveTheme(color);
-}
-
-//get the theme from local storage
-function getTheme() {
-  const theme = localStorage.getItem("theme");
-  if (theme) {
-    changeTheme(theme);
-  }
-}
-
-//save the theme to local storage
-function saveTheme(color) {
-  localStorage.setItem("theme", color);
-}
